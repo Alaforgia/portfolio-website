30b019c12905d490f19c992af2dcbc35354dbe0c
Tony LaForgia <alforgia428@gmail.com>
2022-06-13 17:29:28 -0500

    basic html divs for front page set up

--- pages/HeroPage/index.tsx
+++ pages/HeroPage/index.tsx
@@ -1,5 +1,18 @@
 function HeroPage(): any {
-  return <h1>Hello!</h1>;
+  return (
+    <>
+    <div>
+      <h1>HI. I&apos;m Tony. A Software Engineer</h1>
+      <h3>Photo?</h3>
+    </div>
+    <div>
+      <h3>Projects/Journey</h3>
+    </div>
+    <div>
+      <h3>Small blurb + link</h3>
+    </div>
+    </>
+  );
 }
 
 export default HeroPage;
