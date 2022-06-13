02c8f86857721d839db21f908c8ba5830607b4c4
Tony LaForgia <alforgia428@gmail.com>
2022-06-13 18:53:03 -0500

    Adding styles to each individual page. Will need to correct to universal style

--- src/pages/About/index.tsx
+++ src/pages/About/index.tsx
@@ -1,10 +1,16 @@
-import React from 'react'
+import HeroStyles from "../../components/ui/HeroStyles";
+import Layout from "../../components/layout/Layout";
 
 function About() {
   return (
-    <div><h3>
-      My name is Anthony &quot;Tony&quot; La Forgia</h3></div>
-  )
+    <Layout>
+      <HeroStyles>
+        <div>
+          <h3>My name is Anthony &quot;Tony&quot; La Forgia</h3>
+        </div>
+      </HeroStyles>
+    </Layout>
+  );
 }
 
-export default About
+export default About;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,6 +2,7 @@
 // import Image from "next/image";
 // import styles from "../styles/Home.module.css";
 import HeroPage from "./HeroPage/index";
+import About from "./About/index";
 // import Layout from "../components/layout/Layout";
 
 const HomePage = () => {
