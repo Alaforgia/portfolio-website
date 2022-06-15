1e900ef5fc3913db94eba04c0284626e988f596a
Tony LaForgia <alforgia428@gmail.com>
2022-06-15 18:14:51 -0500

    added recipe detail screen shot to Project

--- src/pages/Projects/index.tsx
+++ src/pages/Projects/index.tsx
@@ -1,6 +1,7 @@
 import Layout from "../../components/layout/Layout";
 import Image from "next/image";
-import profilePic from '../../../public/solo-project-pic1.png'
+import soloProjectPic1 from '../../../public/solo-project-pic1.png'
+import soloProjectPic2 from '../../../public/solo-project-pic2.png'
 
 function Projects() {
   return (
@@ -8,8 +9,12 @@ function Projects() {
       <div>
         <h3>Projects w/ pictures and description</h3>
         <Image 
-        src={profilePic}
-        alt="Picture of Recipe App Solo Project"
+        src={soloProjectPic1}
+        alt="My Recipe View"
+        />
+        <Image 
+        src={soloProjectPic2}
+        alt="Recipe Detail View"
         />
       </div>
     </Layout>
