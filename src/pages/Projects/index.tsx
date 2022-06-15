7a8b067641f74cc0b5e10182f94e6759e5a1b9d5
Tony LaForgia <alforgia428@gmail.com>
2022-06-15 17:35:29 -0500

    added next/image and uploaded unedited screenshot of solo project

--- src/pages/Projects/index.tsx
+++ src/pages/Projects/index.tsx
@@ -1,10 +1,16 @@
 import Layout from "../../components/layout/Layout";
+import Image from "next/image";
+import profilePic from '../../../public/Screen Shot 2022-04-04 at 8.54.29 AM.png'
 
 function Projects() {
   return (
     <Layout>
       <div>
         <h3>Projects w/ pictures and description</h3>
+        <Image 
+        src={profilePic}
+        alt="Picture of Recipe App Solo Project"
+        />
       </div>
     </Layout>
   );
