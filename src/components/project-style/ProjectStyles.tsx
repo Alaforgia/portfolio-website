83b75250732db627e5da8447c5b2f7c882979e21
Tony LaForgia <alforgia428@gmail.com>
2022-06-22 15:54:06 -0500

    text box appear on hover. still need to work out style

--- src/components/project-style/ProjectStyles.tsx
+++ src/components/project-style/ProjectStyles.tsx
@@ -1,9 +1,18 @@
 import styles from "../project-style/ProjectStyles.module.css";
+import Image from "next/image";
+import soloProjectPic1 from "../../../public/solo-project-pic1.png";
+import soloProjectPic2 from "../../../public/solo-project-pic2.png";
 
 function ProjectStyles(props: any) {
   return (
     <>
-      <div className={styles.images}> {props.children} </div>
+      <div className={styles.images}>
+        <Image src={soloProjectPic1} alt="My Recipe View" layout={"intrinsic"} />
+        <Image src={soloProjectPic2} alt="Recipe Detail View" layout={"intrinsic"} />
+      <div className={styles.middle}>
+        <div className={styles.text}>hello</div>
+      </div>
+      </div>
     </>
   );
 }
--- src/pages/Projects/index.tsx
+++ src/pages/Projects/index.tsx
@@ -3,18 +3,23 @@ import Image from "next/image";
 import soloProjectPic1 from "../../../public/solo-project-pic1.png";
 import soloProjectPic2 from "../../../public/solo-project-pic2.png";
 import ProjectStyles from "../../components/project-style/ProjectStyles";
+import styles from "../../components/project-style/ProjectStyles.module.css";
 
-function Projects() {
+function Projects(props: any) {
   return (
     <>
       <Layout>
         <div>
           <h3>Projects w/ pictures and description</h3>
         </div>
-        <ProjectStyles>
+        <ProjectStyles />
+        {/* <ProjectStyles>
           <Image src={soloProjectPic1} alt="My Recipe View" layout={"intrinsic"} />
           <Image src={soloProjectPic2} alt="Recipe Detail View" layout={"intrinsic"} />
         </ProjectStyles>
+        <div className={styles.middle}>
+        <div className={styles.text}>Hello world</div>
+        </div> */}
       </Layout>
     </>
   );
