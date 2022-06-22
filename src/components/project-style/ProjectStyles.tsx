5cf5d72ebbc446f4dbe51ac809bc2d204dadb061
Tony LaForgia <alforgia428@gmail.com>
2022-06-22 17:52:48 -0500

    removed opacity on hover

--- src/components/project-style/ProjectStyles.tsx
+++ src/components/project-style/ProjectStyles.tsx
@@ -7,8 +7,9 @@ function ProjectStyles(props: any) {
   return (
     <>
       <div className={styles.images}>
-        <ShimmerImage src="/solo-project-pic1.png" alt="My Recipe View" width={829} height={1792} />
-        <ShimmerImage src="/solo-project-pic2.png" alt="Recipe Detail View" width={829} height={1792} />
+        {/* Image original width={829} height={1729} */}
+        <ShimmerImage src="/solo-project-pic1.png" alt="My Recipe View" width={629} height={1092} />
+        <ShimmerImage src="/solo-project-pic2.png" alt="Recipe Detail View" width={629} height={1092} />
         <div className={styles.middle}>
           <p className={styles.text}>Solo Project Recipe App</p>
           <p className={styles.subtext}>React.js/Redux/Tailwindcss</p>
