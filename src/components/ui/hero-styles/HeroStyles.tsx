4a08878462645683ab84024e224f9e5d42afa4f1
Tony LaForgia <alforgia428@gmail.com>
2022-06-23 17:01:53 -0500

    file organization

---
+++ src/components/ui/hero-styles/HeroStyles.tsx
@@ -0,0 +1,23 @@
+import classes from "../ui/HeroStyles.module.css";
+import React from "react";
+
+function HeroStyles(props: any) {
+  return (
+    <div className={classes.body}>
+      <div className={classes.card}>
+        <div>
+          <h1>HI. I&apos;m Tony. A Software Engineer</h1>
+          <h3>Photo?</h3>
+        </div>
+        <div>
+          <h3>Projects/Journey</h3>
+        </div>
+        <div>
+          <h3>Small blurb + link</h3>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default HeroStyles;
--- src/pages/HeroPage/index.tsx
+++ src/pages/HeroPage/index.tsx
@@ -1,20 +1,9 @@
 import React from "react";
-import HeroStyles from "../../components/ui/HeroStyles"
+import HeroStyles from "../../components/ui/hero-styles/HeroStyles";
 function HeroPage() {
   return (
     <>
-      <HeroStyles>
-        <div>
-          <h1>HI. I&apos;m Tony. A Software Engineer</h1>
-          <h3>Photo?</h3>
-        </div>
-        <div>
-          <h3>Projects/Journey</h3>
-        </div>
-        <div>
-          <h3>Small blurb + link</h3>
-        </div>
-      </HeroStyles>
+      <HeroStyles />
     </>
   );
 }
