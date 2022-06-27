a3f1487784b98a4d4584528b6b4ca189c9295ae7
Tony LaForgia <alforgia428@gmail.com>
2022-06-27 16:27:47 -0500

    added styling elements to about photo

--- src/components/ui/about-details/AboutDetails.jsx
+++ src/components/ui/about-details/AboutDetails.jsx
@@ -1,9 +1,13 @@
 import React from "react";
 import styles from "../about-details/AboutDetails.module.css";
+import ShimmerImage from "../../../utils/ShimmerImage";
 
 function AboutDetails() {
   return (
     <div className={styles.container}>
+      <div className={styles.photo}>
+        <ShimmerImage src="/bunpic1.png" alt="personal picture" width={400} height={450} />
+      </div>
       <h3 className={styles.text}>My name is Anthony &quot;Tony&quot; La Forgia</h3>
     </div>
   );
