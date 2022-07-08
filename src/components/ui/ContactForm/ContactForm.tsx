9e27a6ecfd24098ca44b228664aebd0fbc48c105
Tony LaForgia <alforgia428@gmail.com>
2022-07-07 22:50:59 -0500

    added p to forms

--- src/components/ui/ContactForm/ContactForm.tsx
+++ src/components/ui/ContactForm/ContactForm.tsx
@@ -4,13 +4,20 @@ import styles from "./ContactForm.module.css";
 function ContactForm() {
   return (
     <div className={styles.contact_container}>
-      <h3>Contact Me</h3>
+      <h1>Contact Me</h1>
       <div className={styles.inputs}>
-      <input type="text"></input>
-      <input type="text"></input>
-      <input type="text"></input>
-      <input type="text"></input>
+        <p>Name</p>
+        <input type="text" placeholder="Name" />
+        <p>E-Mail</p>
+        <input type="text" placeholder="Email" />
+        <p>Subject</p>
+        <input type="text" placeholder="Subject" />
+        <p>Message</p>
+        <div className={styles.text_area}>
+          <textarea placeholder="Message" />
+        </div>
       </div>
+      <button className={styles.btn}>Send</button>
     </div>
   );
 }
