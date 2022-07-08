d07c7e882d7981c66933153091ede65bce2bd46b
Tony LaForgia <alforgia428@gmail.com>
2022-07-07 19:38:06 -0500

    added divs and initial styles to contact form

--- src/components/ui/ContactForm/ContactForm.tsx
+++ src/components/ui/ContactForm/ContactForm.tsx
@@ -1,10 +1,12 @@
-import React from 'react'
-import styles from './ContactForm.module.css'
+import React from "react";
+import styles from "./ContactForm.module.css";
 
 function ContactForm() {
   return (
-    <div>ContactForm</div>
-  )
+    <div className={styles.contact_container}>
+      <div className={styles.forms}>ContactForm</div>
+    </div>
+  );
 }
 
-export default ContactForm
+export default ContactForm;
