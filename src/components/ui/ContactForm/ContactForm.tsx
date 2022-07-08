e998e73755c265984aabf174f54368ec5c2681c0
Tony LaForgia <alforgia428@gmail.com>
2022-07-07 23:51:39 -0500

    ContactForm component clean up

--- src/components/ui/ContactForm/ContactForm.tsx
+++ src/components/ui/ContactForm/ContactForm.tsx
@@ -7,14 +7,14 @@ function ContactForm() {
       <h1>Contact Me</h1>
       <div className={styles.inputs}>
         <p>Name</p>
-        <input type="text" placeholder="Name" />
+        <input type="text" placeholder="Name" required />
         <p>E-Mail</p>
-        <input type="text" placeholder="Email" />
+        <input type="text" placeholder="Email" required/>
         <p>Subject</p>
         <input type="text" placeholder="Subject" />
         <p>Message</p>
         <div className={styles.text_area}>
-          <textarea placeholder="Message" />
+          <textarea placeholder="Message" required/>
         </div>
       </div>
       <div className={styles.btn_container}>
