85632bb0692dcecd940e8ead94fbe0ca8242be14
Tony LaForgia <alforgia428@gmail.com>
2022-07-13 22:28:47 -0500

    added center vertical line and more styling to about

--- src/components/ui/about-details/AboutDetails.jsx
+++ src/components/ui/about-details/AboutDetails.jsx
@@ -9,12 +9,15 @@ function AboutDetails() {
         <ShimmerImage src="/bunpic1.png" alt="personal picture" width={650} height={750} />
         <p className={styles.bunny}>Bunny</p>
       </div>
+      <div className={styles.line}>
+        <p></p>
+      </div>
       <div className={styles.text}>
         <h2>Anthony &quot;Tony&quot; La Forgia</h2>
         <h5>Full-Stack Software Engineer</h5>
         <p>
-          I am new to the software engineering space having recently completed an immersive learning program at Prime
-          Digital Academy located in Minneapolis, MN{" "}
+          I changed careers to be a software engineer in November 2021. I completed an immersive education program at Prime
+          Digital Academy located in Minneapolis, MN. I am committed and love learning as much as I can!
         </p>
       </div>
     </div>
