8c462b3d07d51dbe9b670c09b9116f10c2b723fd
Tony LaForgia <alforgia428@gmail.com>
2022-07-14 01:52:08 -0500

    added background colors and link styling to about

--- src/components/ui/about-details/AboutDetails.jsx
+++ src/components/ui/about-details/AboutDetails.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import styles from "../about-details/AboutDetails.module.css";
 import ShimmerImage from "../../../utils/ShimmerImage";
+import Link from "next/link";
 
 function AboutDetails() {
   return (
@@ -12,13 +13,21 @@ function AboutDetails() {
       <div className={styles.line}>
         <p></p>
       </div>
-      <div className={styles.text}>
-        <h2>Anthony &quot;Tony&quot; La Forgia</h2>
-        <h5>Full-Stack Software Engineer</h5>
-        <p>
-          I decided to change careers to be a software engineer in November 2021 by completing an immersive education program at Prime
-          Digital Academy located in Minneapolis, MN. I am committed and love learning as much as I can!
-        </p>
+      <div className={styles.text_container}>
+        <div className={styles.text}>
+          <h2>Anthony &quot;Tony&quot; La Forgia</h2>
+          <h5>Full-Stack Software Engineer</h5>
+          <p>
+            I decided to change careers to be a software engineer in November 2021 by completing an immersive education
+            program at {""}
+            <Link href="https://www.primeacademy.io/" passHref>
+              <a className={styles.prime_link} target="_blank">
+                Prime Digital Academy
+              </a>
+            </Link>
+            {""} located in Minneapolis, MN. I am committed and love learning as much as I can!
+          </p>
+        </div>
       </div>
     </div>
   );
