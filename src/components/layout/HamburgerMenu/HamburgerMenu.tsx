faa241e1aeb880cdf8432b2e14de82f3f0d3dd3f
Tony LaForgia <alforgia428@gmail.com>
2022-07-20 21:48:16 -0500

    added stlyes to menu

--- src/components/layout/HamburgerMenu/HamburgerMenu.tsx
+++ src/components/layout/HamburgerMenu/HamburgerMenu.tsx
@@ -1,9 +1,38 @@
-import React from 'react'
+import React from "react";
+import styles from "./HamburgerMenu.module.css";
 
-function HamburgerMenu() {
+
+
+function HamburgerMenu({ isNavOpen, setIsNavOpen }: any) {
+
+  const onClickHandler = (): any => {
+    setIsNavOpen(!isNavOpen);
+  };
+
   return (
-    <div>HamburgerMenu</div>
-  )
+    <button className={styles.button} onClick={onClickHandler}>
+      <div className={styles.svg}>
+        <svg
+          className={isNavOpen ? "open text-white" : "text-black"}
+          viewBox="0 0 100 100"
+          fill="none"
+          stroke="currentColor"
+          strokeWidth="5"
+          width="80"
+        >
+          <path
+            className={styles.top}
+            d="m 30,33 h 40 c 3.722839,0 7.5,3.126468 7.5,8.578427 0,5.451959 -2.727029,8.421573 -7.5,8.421573 h -20"
+          />
+          <path className={styles.middle} d="m 30,50 h 40" />
+          <path
+            className={styles.bottom}
+            d="m 70,67 h -40 c 0,0 -7.5,-0.802118 -7.5,-8.365747 0,-7.563629 7.5,-8.634253 7.5,-8.634253 h 20"
+          />
+        </svg>
+      </div>
+    </button>
+  );
 }
 
-export default HamburgerMenu
+export default HamburgerMenu;
